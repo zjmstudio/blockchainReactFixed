2f0a1bd0276caf9048465931071f17fe11f5cbbc
zjmstudio <zjmccartney@gmail.com>
2026-03-06 05:12:48 -0500

    fixed sparklines

--- src/api.js
+++ src/api.js
@@ -8,7 +8,7 @@ export const API = {
     : "/.netlify/functions/trending",
 
   markets: isDev
-    ? "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false"
+    ? "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=true"
     : "/.netlify/functions/markets",
 
   coin: (id) =>
