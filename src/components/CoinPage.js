383a1bd4f44d353505e2f509a88ffbd6d5977cb0
zjmstudio <zjmccartney@gmail.com>
2026-03-09 12:25:21 -0400

    updated coin page and charts

--- src/components/CoinPage.js
+++ src/components/CoinPage.js
@@ -91,7 +91,7 @@ const AreaSparkline = ({ data, activeRange }) => {
 
   const width = 900;
   const height = 260;
-  const padTop = 10;
+  const padTop = 34;
   const bottomY = height;
 
   const min = Math.min(...data.map((pricePoint) => pricePoint.price));
